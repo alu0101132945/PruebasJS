28d9732b9e790089bfc2c731565b30beb92315c6
Carlos Pío Reyes <alu0101132945@ull.edu.es>
2021-03-01 09:56:47 +0000

    Creado botones musicales

---
+++ src/js/index.js
@@ -0,0 +1,77 @@
+const sadmusic = document.querySelector('#musicaTriste')
+function musicatristeplay () {
+  sadmusic.play()
+}
+
+function musicatristepause () {
+  sadmusic.pause()
+}
+
+function musicatristerestart () {
+  sadmusic.currentTime = 0
+}
+
+const happymusic = document.querySelector('#musicaFeliz')
+function musicafelizplay () {
+  happymusic.play()
+}
+
+function musicafelizpause () {
+  happymusic.pause()
+}
+
+function musicafelizrestart () {
+  happymusic.currentTime = 0
+}
+
+const cheerfulmusic = document.querySelector('#musicaAnimada')
+function musicanimadaplay () {
+  cheerfulmusic.play()
+}
+
+function musicanimadapause () {
+  cheerfulmusic.pause()
+}
+
+function musicanimadarestart () {
+  cheerfulmusic.currentTime = 0
+}
+
+const partymusic = document.querySelector('#musicaFiesta')
+function musicafiestaplay () {
+  partymusic.play()
+}
+
+function musicafiestapause () {
+  partymusic.pause()
+}
+
+function musicafiestarestart () {
+  partymusic.currentTime = 0
+}
+
+const pillarmusic = document.querySelector('#musicaPilar')
+function musicapilarplay () {
+  pillarmusic.play()
+}
+
+function musicapilarpause () {
+  pillarmusic.pause()
+}
+
+function musicapilarrestart () {
+  pillarmusic.currentTime = 0
+}
+
+const torturemusic = document.querySelector('#musicaTortura')
+function musicatorturaplay () {
+  torturemusic.play()
+}
+
+function musicatorturapause () {
+  torturemusic.pause()
+}
+
+function musicatorturarestart () {
+  torturemusic.currentTime = 0
+}
